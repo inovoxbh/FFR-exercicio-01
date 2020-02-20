ab046689597a228f0fe7902e8ffba4dc95ec8320
Bruno G Alcantara <inovox@gmail.com>
2020-02-19 22:04:14 -0300

    exclusao do conteudo original do boilerplate

--- src/App.js
+++ src/App.js
@@ -10,22 +10,6 @@ import CompRodape from './CompRodape.js'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <hr></hr>
       <div>
         <h1> Cabeçalho </h1>
         <CompCabecalho />
